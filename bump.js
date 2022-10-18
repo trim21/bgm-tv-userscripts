20aafcdbb64cdfaf0ae3e62441552175d2f79723
Trim21 <trim21.me@gmail.com>
2022-10-18 19:51:43 +0800

    refactor: simplify bump script

--- bump.js
+++ bump.js
@@ -5,23 +5,18 @@ const { execSync } = require('child_process');
 const semver = require('semver');
 const { hideBin } = require('yargs/helpers');
 
-const argv = hideBin(process.argv);
-
-const [pkg, ...bump] = argv;
-
-const pkgRoot = path.join(__dirname, 'packages', pkg);
-
-const pkgFilePath = path.join(pkgRoot, 'package.json');
-
+const [pkg, ...bump] = hideBin(process.argv);
+const pkgFilePath = path.join(path.join(__dirname, 'packages', pkg), 'package.json');
 const packageJSON = JSON.parse(fs.readFileSync(pkgFilePath).toString());
 
-let newVersion;
-
-if (bump.length === 1 && semver.valid(bump[0], {})) {
-  newVersion = bump[0];
-} else {
-  newVersion = semver.inc(packageJSON.version, ...bump);
+function getNewVersion() {
+  if (bump.length === 1 && semver.valid(bump[0], {})) {
+    return bump[0];
+  }
+  return semver.inc(packageJSON.version, ...bump);
 }
+
+const newVersion = getNewVersion();
 
 if (!newVersion) {
   console.log('no version, re-run');
@@ -35,9 +30,7 @@ packageJSON.version = newVersion;
 fs.writeFileSync(pkgFilePath, JSON.stringify(packageJSON, undefined, 2));
 
 execSync('git add .', { encoding: 'utf8' });
-
 execSync(`git commit -m "release: bump \`${packageJSON.name}\` to ${newVersion}"`, { encoding: 'utf8' });
-
 execSync(`git tag -a -m "${packageJSON.name}/v${newVersion}" "${packageJSON.name}/v${newVersion}"`, {
   encoding: 'utf8',
 });
