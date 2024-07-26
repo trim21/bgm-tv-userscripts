1b52fe7cb637993c323035314d5ab4db2b46b402
Wanten <41904684+WantenMN@users.noreply.github.com>
2024-07-26 10:19:39 +0800

    feat: applyThemeBasedOnCookie (#241)

--- packages/wiki-rev-diff/src/ui.ts
+++ packages/wiki-rev-diff/src/ui.ts
@@ -1,20 +1,22 @@
 import * as Diff2Html from 'diff2html';
 import * as $ from 'jquery';
-import type { OutputFormatType } from 'diff2html/lib/types';
+import type { ColorSchemeType, OutputFormatType } from 'diff2html/lib/types';
 
 import { configKey } from './config';
 import type { Commit } from './model';
 import { diff } from './differ';
+import { getCookie } from './utils';
 
 export async function render(revOld: Commit, revNew: Commit): Promise<void> {
   let outputFormat = (await GM.getValue(configKey)) as OutputFormatType | undefined;
   if (!outputFormat) {
     outputFormat = 'line-by-line';
   }
+  const colorScheme = getCookie('chii_theme') as ColorSchemeType;
 
   const patch = diff(revOld, revNew, outputFormat);
 
-  const html = Diff2Html.html(patch, { outputFormat });
+  const html = Diff2Html.html(patch, { outputFormat, colorScheme });
   const elID = `show-diff-view-${outputFormat}`;
 
   show('');
